7c987de337525147d5bb6a83092259f2fec4e5aa
Allan Bontempo T. Siqueira <allan.siqueira@totvspartners.com.br>
2024-10-20 22:48:21 -0300

    Inicio do projeto backend

---
+++ backend/src/main/java/br/com/caliandra/caliandra/CaliandraApplication.java
@@ -0,0 +1,13 @@
+package br.com.caliandra.caliandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaliandraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/caliandra/caliandra/CaliandraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caliandra.caliandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaliandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

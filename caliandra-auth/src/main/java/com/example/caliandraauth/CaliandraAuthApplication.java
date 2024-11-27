1be1623b4411cba279961376dd679e4601a2ba39
Allan Bontempo <allanbt@hotmail.com>
2024-11-27 20:54:31 -0300

    auth spring criado

---
+++ caliandra-auth/src/main/java/com/example/caliandraauth/CaliandraAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.caliandraauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliandraAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaliandraAuthApplication.class, args);
+	}
+
+}
---
+++ caliandra-auth/src/test/java/com/example/caliandraauth/CaliandraAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caliandraauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaliandraAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
